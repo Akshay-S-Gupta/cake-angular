5ebe165ddfa4f29ab4dff2ecdd373c259e2d787e
akshay-s-gupta <akshaysgupta2003@gmail.com>
2025-10-08 10:00:34 +0530

    src del

--- src/app/services/form.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class FormService {
-  validateEmail(email: string): boolean { return /[^@\s]+@[^@\s]+\.[^@\s]+/.test(email); }
-}
-
-
