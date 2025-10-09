70d6923eaca527c909be1055d6c0709e2b6ca123
anushkakulkarni07 <anushka.u.kulkarni19@gmail.com>
2025-10-09 09:26:50 +0530

    Newsletter added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,14 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { NewsletterComponent } from './components/newsletter/newsletter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, NewsletterComponent, FooterComponent],
   template: `
     <app-header></app-header>
     <router-outlet></router-outlet>
+    <app-newsletter></app-newsletter>
     <app-footer></app-footer>
   `
 })
